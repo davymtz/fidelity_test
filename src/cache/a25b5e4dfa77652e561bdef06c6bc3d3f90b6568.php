7b02446854709f9c5fe287cc8c6521f8501fcd98
David Mtz <davymtz91@gnmail.com>
2022-09-12 23:50:54 -0500

    Actualizaciones a gitignore

--- src/cache/a25b5e4dfa77652e561bdef06c6bc3d3f90b6568.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection("content"); ?>
-<section>
-    <h1>Esto es un pequeño render</h1>
-</section>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make("layout", array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
